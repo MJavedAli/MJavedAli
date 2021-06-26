cf40a9717d30e75e03443ce85f240e11293209cc
Ali <jali@softwareag.com>
2021-06-26 23:37:57 +0530

    async fix

--- index.js
+++ index.js
@@ -19,53 +19,7 @@ let DATA = {
   }),
 };
 
-async function setWeatherInformation() {
-
-  let r = await fetch(`https://openweathermap.org/data/2.5/find?q=KOlkata&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`)
-  let sun_rise_set = await fetch('https://api.sunrise-sunset.org/json?lat=22.5726&lng=88.3639&formatted=0');
-  let rjson = await r.json();
-  let rsunriseset = await sun_rise_set.json();
-  DATA.city_temperature = Math.round(await (rjson.list[0].main.temp) / 10);
-  DATA.city_weather = await rjson.list[0].weather[0].description;
-  DATA.city_weather_icon = await rjson.list[0].weather[0].icon;
-
-  let dtsr = moment(rsunriseset.results.sunrise, 'YYYY/MM/DD HH:mm:ss ZZ').tz('Asia/Kolkata').format('HH:mm');
-  let dtss = moment(rsunriseset.results.sunset, 'YYYY/MM/DD HH:mm:ss ZZ').tz('Asia/Kolkata').format('HH:mm');
-  DATA.sun_rise = dtsr + ' AM';
-  DATA.sun_set = dtss + ' PM';
-
-  // .then((r) => r.json())
-  // .then(r => {
-  //   DATA.city_temperature = Math.round(r.list[0].main.temp);
-  //   DATA.city_weather = r.list[0].weather[0].description;
-  //   DATA.city_weather_icon = r.list[0].weather[0].icon;
-  //   DATA.sun_rise = new Date(r.list[0].sys.sunrise * 1000).toLocaleString('en-GB', {
-  //     hour: '2-digit',
-  //     minute: '2-digit',
-  //     timeZone: 'Asia/Kolkata',
-  //   });
-  //   DATA.sun_set = new Date(r.list[0].sys.sunset * 1000).toLocaleString('en-GB', {
-  //     hour: '2-digit',
-  //     minute: '2-digit',
-  //     timeZone: 'Asia/Kolkata',
-  //   });
-  // });
-}
-
-async function setInstagramPosts() {
-  const instagramImages = await puppeteerService.getLatestInstagramPostsFromAccount('thisiskolkata', 3);
-  DATA.img1 = instagramImages[0];
-  DATA.img2 = instagramImages[1];
-  DATA.img3 = instagramImages[2];
-}
-
-async function generateReadMe() {
-  await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
-    if (err) throw err;
-    const output = Mustache.render(data.toString(), DATA);
-    fs.writeFileSync('README.md', output);
-  });
-}
+await action();
 
 async function action() {
   /**
@@ -89,4 +43,36 @@ async function action() {
   await puppeteerService.close();
 }
 
-action();
+
+async function setWeatherInformation() {
+
+  let r = await fetch(`https://openweathermap.org/data/2.5/find?q=KOlkata&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`)
+  let sun_rise_set = await fetch('https://api.sunrise-sunset.org/json?lat=22.5726&lng=88.3639&formatted=0');
+  let rjson = await r.json();
+  let rsunriseset = await sun_rise_set.json();
+  DATA.city_temperature = Math.round(await (rjson.list[0].main.temp) / 10);
+  DATA.city_weather = await rjson.list[0].weather[0].description;
+  DATA.city_weather_icon = await rjson.list[0].weather[0].icon;
+
+  let dtsr = moment(rsunriseset.results.sunrise, 'YYYY/MM/DD HH:mm:ss ZZ').tz('Asia/Kolkata').format('HH:mm');
+  let dtss = moment(rsunriseset.results.sunset, 'YYYY/MM/DD HH:mm:ss ZZ').tz('Asia/Kolkata').format('HH:mm');
+  DATA.sun_rise = dtsr + ' AM';
+  DATA.sun_set = dtss + ' PM';
+
+}
+
+async function setInstagramPosts() {
+  const instagramImages = await puppeteerService.getLatestInstagramPostsFromAccount('thisiskolkata', 3);
+  DATA.img1 = instagramImages[0];
+  DATA.img2 = instagramImages[1];
+  DATA.img3 = instagramImages[2];
+}
+
+async function generateReadMe() {
+  await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+    if (err) throw err;
+    const output = Mustache.render(data.toString(), DATA);
+    fs.writeFileSync('README.md', output);
+  });
+}
+
