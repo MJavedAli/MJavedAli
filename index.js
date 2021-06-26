2cc2354ff49d83d89906b6c9dbcb791db7647148
Ali <jali@softwareag.com>
2021-06-26 23:30:38 +0530

    updated index

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const Mustache = require('mustache');
 const fetch = require('node-fetch');
 const fs = require('fs');
+const moment = require('moment-timezone');
 const puppeteerService = require('./services/puppeteer.service');
 
 const MUSTACHE_MAIN_DIR = './main.mustache';
@@ -20,26 +21,35 @@ let DATA = {
 
 async function setWeatherInformation() {
 
-  await fetch(
-    `https://openweathermap.org/data/2.5/find?q=KOlkata&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`
+  let r = await fetch(`https://openweathermap.org/data/2.5/find?q=KOlkata&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`)
+  let sun_rise_set = await fetch('https://api.sunrise-sunset.org/json?lat=22.5726&lng=88.3639&formatted=0');
+  let rjson = await r.json();
+  let rsunriseset = await sun_rise_set.json();
+  DATA.city_temperature = Math.round(await (rjson.list[0].main.temp) / 10);
+  DATA.city_weather = await rjson.list[0].weather[0].description;
+  DATA.city_weather_icon = await rjson.list[0].weather[0].icon;
 
-  )
-    .then((r) => r.json())
-    .then(r => {
-      DATA.city_temperature = Math.round(await r.list[0].main.temp);
-      DATA.city_weather = await r.list[0].weather[0].description;
-      DATA.city_weather_icon = await r.list[0].weather[0].icon;
-      // DATA.sun_rise = new Date(r.list[0].sys.sunrise * 1000).toLocaleString('en-GB', {
-      //   hour: '2-digit',
-      //   minute: '2-digit',
-      //   timeZone: 'Asia/Kolkata',
-      // });
-      // DATA.sun_set = new Date(r.list[0].sys.sunset * 1000).toLocaleString('en-GB', {
-      //   hour: '2-digit',
-      //   minute: '2-digit',
-      //   timeZone: 'Asia/Kolkata',
-      // });
-    });
+  let dtsr = moment(rsunriseset.results.sunrise, 'YYYY/MM/DD HH:mm:ss ZZ').tz('Asia/Kolkata').format('HH:mm');
+  let dtss = moment(rsunriseset.results.sunset, 'YYYY/MM/DD HH:mm:ss ZZ').tz('Asia/Kolkata').format('HH:mm');
+  DATA.sun_rise = dtsr + ' AM';
+  DATA.sun_set = dtss + ' PM';
+
+  // .then((r) => r.json())
+  // .then(r => {
+  //   DATA.city_temperature = Math.round(r.list[0].main.temp);
+  //   DATA.city_weather = r.list[0].weather[0].description;
+  //   DATA.city_weather_icon = r.list[0].weather[0].icon;
+  //   DATA.sun_rise = new Date(r.list[0].sys.sunrise * 1000).toLocaleString('en-GB', {
+  //     hour: '2-digit',
+  //     minute: '2-digit',
+  //     timeZone: 'Asia/Kolkata',
+  //   });
+  //   DATA.sun_set = new Date(r.list[0].sys.sunset * 1000).toLocaleString('en-GB', {
+  //     hour: '2-digit',
+  //     minute: '2-digit',
+  //     timeZone: 'Asia/Kolkata',
+  //   });
+  // });
 }
 
 async function setInstagramPosts() {
